c274543716f84ecfb5debdd432f10b46134228d3
cyccc95 <cyccc95@naver.com>
2023-02-12 18:19:31 +0900

    mysql 연결을 위한 db_config 작성, npm install mysql

---
+++ ais-decoding/config/db_config.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const config = require("./db_config.json");
+
+let pool = mysql.createPool(config);
+
+const getConnection = (callback) => {
+  pool.getConnection((err, conn) => {
+    if (!err) {
+      callback(conn);
+    } else {
+      console.log(err);
+    }
+  });
+};
+
+module.exports = getConnection;
