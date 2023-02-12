bafe0e66ae3e59803ddae18ca7b8a3d8884d4c78
cyccc95 <cyccc95@naver.com>
2023-02-12 21:54:43 +0900

    Title.js 작성

--- front-end/src/components/sidebar/Title.js
+++ front-end/src/components/sidebar/Title.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TitleStyle = styled.div`
+  height: 15vh;
+  text-align: center;
+  font-size: 40px;
+  padding-top: 10px;
+`;
 
 const Title = () => {
-  return <div></div>;
+  return (
+    <TitleStyle>
+      선박 위치
+      <br />
+      예측 서비스
+    </TitleStyle>
+  );
 };
 
 export default Title;
