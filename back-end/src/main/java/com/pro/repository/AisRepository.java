48c39402063f6aba5bfb17d7007df5604536ae6a
cyccc95 <cyccc95@naver.com>
2023-02-14 12:41:49 +0900

    query 줄 변경

--- back-end/src/main/java/com/pro/repository/AisRepository.java
+++ back-end/src/main/java/com/pro/repository/AisRepository.java
@@ -15,7 +15,8 @@ public interface AisRepository extends JpaRepository<Ais, AisKey> {
     List<Ais> findAllRecentAis();
 
     @Query(value = "select * from (\n" +
-            "\tselect ais.signal_date, ais.cog, ais.posX, ais.posY, ais.sog, ais.mmsi, ship.shipType from ais inner join ship ship on ais.mmsi = ship.mmsi and ship.shipType = ?1 order by ais.mmsi, signal_date DESC limit 18446744073709551615\n" +
+            "\tselect ais.signal_date, ais.cog, ais.posX, ais.posY, ais.sog, ais.mmsi, ship.shipType from ais inner join" +
+            " ship ship on ais.mmsi = ship.mmsi and ship.shipType = ?1 order by ais.mmsi, signal_date DESC limit 18446744073709551615\n" +
             "    ) as recent_ais group by mmsi", nativeQuery = true)
     List<Ais> findAllRecentAisByShipType(Integer shipType);
 
@@ -25,7 +26,8 @@ public interface AisRepository extends JpaRepository<Ais, AisKey> {
     @Query(value = "select * from ais where mmsi = ?1 order by signal_date", nativeQuery = true)
     List<Ais> findAllAisByMmsi(Integer mmsi);
 
-    @Query(value = "select * from (select * from ais order by mmsi, signal_date DESC limit 18446744073709551615) as recent_ais group by mmsi having timestampdiff(MINUTE, signal_date, current_timestamp) >= 5", nativeQuery = true)
+    @Query(value = "select * from (select * from ais order by mmsi, signal_date DESC limit 18446744073709551615) as recent_ais" +
+                    " group by mmsi having timestampdiff(MINUTE, signal_date, current_timestamp) >= 5", nativeQuery = true)
     List<Ais> findAllAisLoss();
 
     @Query(value = "select * from ais where mmsi=?1 order by signal_date DESC limit 2", nativeQuery = true)
